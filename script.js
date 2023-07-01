35106b395ab735c4d87e2e7b85cebf64b885b069
twiickcom <twiick.com@gmail.com>
2023-06-30 22:02:06 -0500

    Se incluye en el código las instrucciones para llamar elementos html del archivo index.html desde el archivo script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+// console.log("Hello, world!");
+var titulo=document.querySelector("h1");
+var parrafo=document.querySelector("p");
+var parrafito=document.querySelector(".parrafito");
+var parrafoid=document.getElementById("pid");
+var input=document.querySelector("input");
+
+input.value="Este es el contenido del Input";
+
+console.log({
+    titulo,
+    parrafo,
+    parrafito,
+    parrafoid,
+    input
+});
