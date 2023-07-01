c4e7eb3c43c9ad29e4f492b474ed0acab43adb2f
twiickcom <twiick.com@gmail.com>
2023-07-01 10:09:55 -0500

    Se agregan instrucciones en el documento index.html, para leer eventos en JavaScript. Se eliminan varias lineas de código y se plantea un ejerciciode sumas con dos input y un boton

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 // console.log("Hello, world!");
 var titulo=document.querySelector("h1");
-var parrafo=document.querySelector("p");
-var parrafito=document.querySelector(".parrafito");
-var parrafoid=document.getElementById("pid");
-var input=document.querySelector("input");
+const input1=document.getElementById("calculo1");
+const input2=document.getElementById("calculo2");
+const btn=document.getElementById("btnCalcular");
 
-input.value="Este es el contenido del Input";
+let resultado=0;
+let textoResultado=document.getElementById("resultado");
 
-// Cambia el contenido de un elemento en HTML (Incluye nuevos elementos HTML)
-titulo.innerHTML="Este es el<br>nuevo titulo";
-// Cambia el contenido de un elemento en HTML (Solo texto)
-parrafoid.innerText="Se cambia el Texto";
-// Obtiene un atributo específico
-console.log(parrafo.getAttribute("tipoTexto"));
-// Cambia el valor de un atributo en especifico
-parrafo.setAttribute("tipoTexto", "secundario");
-// Agrga o quita clases a un elemento
-parrafo.classList.add("verde");
-parrafo.classList.add("red");
-parrafo.classList.toggle("azul");
-//Devuelve true o false si el elemento tiene la vlade indicada
-console.log(parrafo.classList.contains("verde"));
 
-var imagen=document.createElement("img");
-imagen.setAttribute("src", "https://c4.wallpaperflare.com/wallpaper/892/692/922/howl-s-moving-castle-studio-ghibli-fantasy-art-clouds-daylight-hd-wallpaper-preview.jpg");
-parrafoid.appendChild(imagen);
+let valor1=0;
+let valor2=0;
 
-console.log({
-    titulo,
-    parrafo,
-    parrafito,
-    parrafoid,
-    input
-});
+function recibirValor(input){
+    if(input=="input1"){
+        valor1=parseInt(input1.value);
+    }
+    if(input=="input2"){
+        valor2=parseInt(input2.value);
+    }
+}
+
+function btnOnClick(){
+    resultado=valor1+valor2;
+    textoResultado.innerText="El resultado de la suma es: "+resultado;
+}
