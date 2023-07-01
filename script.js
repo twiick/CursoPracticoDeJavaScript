171eb9793be26f300e7a5db10f88f64cc831979e
twiickcom <twiick.com@gmail.com>
2023-07-01 09:02:41 -0500

    Se agregan las instrucciones para manipular las clases y atributos de los elementos HTML desde JavaScript

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ var input=document.querySelector("input");
 
 input.value="Este es el contenido del Input";
 
+// Cambia el contenido de un elemento en HTML (Incluye nuevos elementos HTML)
+titulo.innerHTML="Este es el<br>nuevo titulo";
+// Cambia el contenido de un elemento en HTML (Solo texto)
+parrafoid.innerText="Se cambia el Texto";
+// Obtiene un atributo específico
+console.log(parrafo.getAttribute("tipoTexto"));
+// Cambia el valor de un atributo en especifico
+parrafo.setAttribute("tipoTexto", "secundario");
+// Agrga o quita clases a un elemento
+parrafo.classList.add("verde");
+parrafo.classList.add("red");
+parrafo.classList.toggle("azul");
+//Devuelve true o false si el elemento tiene la vlade indicada
+console.log(parrafo.classList.contains("verde"));
+
+var imagen=document.createElement("img");
+imagen.setAttribute("src", "https://c4.wallpaperflare.com/wallpaper/892/692/922/howl-s-moving-castle-studio-ghibli-fantasy-art-clouds-daylight-hd-wallpaper-preview.jpg");
+parrafoid.appendChild(imagen);
+
 console.log({
     titulo,
     parrafo,
